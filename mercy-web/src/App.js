8b25a95800c87d945d6d320de5ec3709d5f42312
My-Robot20 <danbrasafe@gmail.com>
2023-09-17 04:35:42 -0400

    Mercy Skeleton

---
+++ mercy-web/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='headerM'>
+     
+      <img src='/images/MercyB1B.png' className="App-logo" alt="logo" />
+      </h1>
+      
+        
+      <p className='mission'>
+          The Greatest Concepts Need Execution In Our Lifetime 
+  </p>
+        {/*<p className='mission'>
+          Be Grateful For The Present And Hope For A Limitless Future 
+  </p> */}
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
